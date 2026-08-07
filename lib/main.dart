b7580ac8ad8bc3b3040efe1b0f7b61f83f949edf
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-08-07 19:56:38 +0545

    Learn about shadow

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,12 @@ void main() {
 class MyApplication extends StatelessWidget {
   const MyApplication({super.key});
 
-  //final String name = "Arun";
+  // final String name = "Arun";
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
@@ -23,19 +24,33 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(child : Text("Hi guys my name is Arun Sagar Rai,")),
-
+      body: Center(
+        child: Text(
+          "My First Application",
+          style: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w500,
+            height: 1,
+            letterSpacing: 2,
+            wordSpacing: 12,
+            shadows: [Shadow(color: Colors.black,
+            blurRadius: 10, offset: Offset(-30, -30),),],
+            /*decoration: TextDecoration.combine([
+              TextDecoration.underline,
+              //TextDecoration.overline,
+              //TextDecoration.lineThrough
+            ]),
+            decorationColor: Colors.red,
+            decorationStyle: TextDecorationStyle.dashed,*/
+          ),
+        ),
+      ),
     );
   }
 }
 
-
-
 // Stateless widget
-/*Its static widget - whose state doesnot change*/
-
-
-
+/* Its static widget - whose state does not change */
 
 // Stateful widget
-/*Its dynami widget - whose state can change*/
+/* Its dynamic widget - whose state can change */
